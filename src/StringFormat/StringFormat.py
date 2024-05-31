8daef2ab51abb6ea048db64fc5ce94feffe16fb6
Paul Hively <pbhively@gmail.com>
2024-05-31 14:07:37 -0500

    Added StringFormat.trim_whitespace

--- src/StringFormat/StringFormat.py
+++ src/StringFormat/StringFormat.py
@@ -3,3 +3,7 @@ import re
 
 def strip_non_numeric(string_in):
     return re.sub("[^0-9]", "", string_in)
+
+
+def trim_whitespace(string_in):
+    return string_in.strip()
--- src/StringFormat/StringFormat_test.py
+++ src/StringFormat/StringFormat_test.py
@@ -1,8 +1,14 @@
 import StringFormat
-import unittest
 
 
 def test_strip_non_numeric():
     assert StringFormat.strip_non_numeric("a123b") == "123"
     assert StringFormat.strip_non_numeric("a 123b") == "123"
     assert StringFormat.strip_non_numeric("a1@23b") == "123"
+
+
+def test_trim_witespace():
+    assert StringFormat.trim_whitespace("Aa1!Bb.'2@Cc3#") == "Aa1!Bb.'2@Cc3#"
+    assert StringFormat.trim_whitespace(" SS ") == "SS"
+    assert StringFormat.trim_whitespace(" \t\n\rABC\t\n\r ") == "ABC"
+    assert StringFormat.trim_whitespace("  ") == ""
