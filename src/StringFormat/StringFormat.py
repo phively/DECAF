59e1c92fa276eb4c4bba4d9f73c9c9664d5fa42d
Paul Hively <pbhively@gmail.com>
2024-05-31 10:37:16 -0500

    Added Black formatter

--- src/StringFormat/StringFormat.py
+++ src/StringFormat/StringFormat.py
@@ -1,4 +1,5 @@
 import re
 
-def strip_non_numeric(stringIn):
-    return re.sub("[^0-9]", "", stringIn)
+
+def strip_non_numeric(string_in):
+    return re.sub("[^0-9]", "", string_in)
--- src/StringFormat/StringFormat_test.py
+++ src/StringFormat/StringFormat_test.py
@@ -1,6 +1,7 @@
 import StringFormat
 import unittest
 
+
 def test_strip_non_numeric():
     assert StringFormat.strip_non_numeric("a123b") == "123"
     assert StringFormat.strip_non_numeric("a 123b") == "123"
