e7e3f81dde20fa74d68c944ab495e93f1abca5a0
Paul Hively <pbhively@gmail.com>
2024-06-04 16:54:04 -0500

    ConfigReader tweaks - Added parse_functions to extract function values as list - Refactored test script

--- src/ConfigReader/ConfigReader.py
+++ src/ConfigReader/ConfigReader.py
@@ -6,3 +6,10 @@ def read_config(config_file):
     cp = ConfigParser()
     cp.read(config_file)
     return cp
+
+
+# Parse [functions] value into list
+def parse_functions(cp):
+    # Read value and strip whitespace
+    fns = cp["control"]["functions"].replace(" ", "")
+    return fns.split(",")
--- src/ConfigReader/ConfigReader_test.py
+++ src/ConfigReader/ConfigReader_test.py
@@ -1,14 +1,23 @@
 import ConfigReader
 
-path = "src/ConfigReader/"
-hw = ConfigReader.read_config(path + "config_sample/hello_world.ini")
-pt = ConfigReader.read_config(path + "config_sample/pythagorean_theorem.ini")
+
+def read_test_configs(path="src/ConfigReader/"):
+    hw = ConfigReader.read_config(path + "config_sample/hello_world.ini")
+    pt = ConfigReader.read_config(path + "config_sample/pythagorean_theorem.ini")
+    return hw, pt
 
 
 def test_read_config():
+    hw, pt = read_test_configs()
     assert hw.sections() == ["info", "control"]
     assert list(hw["info"]) == ["output_type"]
     assert hw["info"]["output_type"] == "string"
     assert pt.sections() == ["info", "control"]
     assert list(pt["control"]) == ["functions"]
     assert pt["control"]["functions"] == "tuplesquare, sum, math.sqrt"
+
+
+def test_parse_functions():
+    hw, pt = read_test_configs()
+    assert ConfigReader.parse_functions(hw) == ["print"]
+    assert ConfigReader.parse_functions(pt) == ["tuplesquare", "sum", "math.sqrt"]
