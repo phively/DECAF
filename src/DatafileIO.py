cea9cb1bcadff3271b71c6cf5e630c4c9c0afa6a
Paul Hively <pbhively@gmail.com>
2024-09-12 15:56:13 -0500

    DatafileIO unit tests - New _load_files function to allow for unit testing (no ConfigReader) - Moved previous unit tests into DatafileIO_int_test.py - First tests pass for #2

--- src/DatafileIO.py
+++ src/DatafileIO.py
@@ -2,8 +2,25 @@ import ConfigReader as cr
 import pandas as pd
 
 
-def _load_file(filepath):
-    return pd.read_csv(filepath)
+def _load_files(fp1, cn1="", fp2="", cn2=""):
+    # Switching
+    nfiles = int(fp1 != "") + int(fp2 != "")
+    ncols = int(cn1 != "") + int(cn2 != "")
+    # 1 file, 0-3 col
+    if nfiles == 1:
+        if ncols == 0:
+            return pd.read_csv(fp1)
+        elif ncols == 1:
+            return pd.read_csv(fp1)[cn1]
+        elif ncols == 2:
+            return pd.read_csv(fp1)[[cn1, cn2]]
+    elif nfiles == 2:
+        # 2 file, 2 col
+        try:
+            return pd.read_csv(fp1)[cn1], pd.read_csv(fp2)[cn2]
+        # 2 file, 1 or 2 col: INVALID
+        except ValueError:
+            return
 
 
 def _write_file(filepath):
@@ -18,22 +35,4 @@ def load_files_from_ini(ini_path):
     cn1 = cp["control"]["column1"]
     fp2 = cp["control"]["file2"]
     cn2 = cp["control"]["column2"]
-    # Switching
-    nfiles = int(fp1 != "") + int(fp2 != "")
-    ncols = int(cn1 != "") + int(cn2 != "")
-    # 1 file, 0-3 col
-    if nfiles == 1:
-        if ncols == 0:
-            return _load_file(fp1)
-        elif ncols == 1:
-            return _load_file(fp1)[cn1]
-        elif ncols == 2:
-            return _load_file(fp1)[cn1, cn2]
-    elif nfiles == 2:
-        # 2 file, 2 col
-        try:
-            return
-        # 2 file, 1 or 2 col: INVALID
-        except ValueError:
-            return
-    return
+    return _load_files(fp1, cn1, fp2, cn2)
--- src/tests/unit/DatafileIO_test.py
+++ src/tests/unit/DatafileIO_test.py
@@ -1,26 +1,23 @@
 import DatafileIO as dio
-import ConfigReader as cr
 import pandas as pd
 
-config_path = "src/tests/config/"
+config_path = "src/tests/"
 
 
 # Return full path to test ini and test data file
-def _get_paths(ini):
-    ini_path = config_path + ini
-    cp = cr.read_config(ini_path)
-    data_path = cp["control"]["file1"]
-    return ini_path, data_path
-
-
-def test_load_files_from_ini():
-    ini_path, data_path = _get_paths("load_pythagorean_triples_all.ini")
-    assert dio.load_files_from_ini(ini_path).equals(pd.read_csv(data_path))
+def test_load_files():
+    data_path = config_path + "data/pythagorean_triples.csv"
+    assert dio._load_files(data_path, "", "", "").equals(pd.read_csv(data_path))
     # 1 file 1 col
-    ini_path, data_path = _get_paths("load_pythagorean_triples_cols_c.ini")
-
+    assert dio._load_files(data_path, "c", "", "").equals(pd.read_csv(data_path)["c"])
     # 1 file 2 col
-    ini_path, data_path = _get_paths("load_pythagorean_triples_cols_ab.ini")
-
+    assert dio._load_files(data_path, "a", "", "b").equals(
+        pd.read_csv(data_path)[["a", "b"]]
+    )
     # 2 file 2 col
-    ini_path, data_path = _get_paths("load_fuzzy_match_companies.ini")
+    data_path = config_path + "data/fuzzy_match_companies.csv"
+    rn, nn = dio._load_files(
+        fp1=data_path, cn1="reference_name", fp2=data_path, cn2="new_name"
+    )
+    assert rn.equals(pd.read_csv(data_path)["reference_name"])
+    assert nn.equals(pd.read_csv(data_path)["new_name"])
