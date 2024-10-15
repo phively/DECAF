47ffab9935946cef6228161c630eec7099da5cba
Paul Hively <pbhively@gmail.com>
2024-10-15 15:21:42 -0500

    Fuzzy match company e2e testing - Added file path references to fuzzy_match_company.ini - Added additional test cases to FuzzyMatch_int_test - Closes #1

--- src/tests/integration/FuzzyMatch_int_test.py
+++ src/tests/integration/FuzzyMatch_int_test.py
@@ -71,12 +71,12 @@ def test_company_fuzzy_match_from_config():
 def test_company_fuzzy_match_nested_config():
     # Load data
     companies = load_match_data()
-    path = "src/tests/config/processing/fuzzy_match_company.ini"
-    config = cr.read_config(path)
+    inipath = "src/tests/config/processing/fuzzy_match_company.ini"
+    config = cr.read_config(inipath)
 
     # Process functions
     match_fns = cr.parse_functions(config)
-    clean_fns = dio._read_cleaning_from_ini(path)
+    clean_fns = dio._read_cleaning_from_ini(inipath)
     assert match_fns is not None
     assert clean_fns is not None
     ref_name = cf.eval_functions_list(companies["reference_name"], clean_fns)
@@ -89,5 +89,38 @@ def test_company_fuzzy_match_nested_config():
 
 
 def test_company_fuzzy_match_e2e():
-    # Load data
-    assert True
+    # Params
+    datapath = "src/tests/data/fuzzy_match_companies.csv"
+    inipath = "src/tests/config/processing/fuzzy_match_company.ini"
+
+    # Load data and config
+    companies = dio.load_file(datapath)
+    config = cr.read_config(inipath)
+    ex = dio.load_files_from_ini(inipath)
+    assert ex.equals(pd.read_csv(datapath)["expected"])
+
+    # Check params
+    match_threshold = int(config["parameters"]["match_threshold"])
+    assert match_threshold == 80
+    filesuffix = config["info"]["name"]
+    assert filesuffix == "_fuzzy_match_company"
+
+    # Process functions
+    match_fns = cr.parse_functions(config)
+    clean_fns = dio._read_cleaning_from_ini(inipath)
+    assert match_fns is not None
+    assert clean_fns is not None
+    ref_name = cf.eval_functions_list(companies["reference_name"], clean_fns)
+    new_name = cf.eval_functions_list(companies["new_name"], clean_fns)
+
+    # Append and check scores
+    companies["scores"] = fm.fuzzy_match_pairwise(ref_name, new_name)
+    companies["result"] = np.where(
+        fm.score_threshold(companies["scores"], match_threshold), "match", "new"
+    )
+    assert ex.to_list() == companies["result"].to_list()
+
+    # Write data to csv
+    dio._write_file(companies, filepath=datapath + filesuffix + "_test.csv", type="csv")
+    saved_companies = pd.read_csv(datapath + filesuffix + "_test.csv")
+    assert saved_companies["scores"].equals(companies["scores"])
